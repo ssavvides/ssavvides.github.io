9a954fd8bf86d47732ef76dc037aa900b418ac85
Savvas Savvides <savvas.savvides@fortanix.com>
2020-09-05 22:03:55 -0700

    rolling profile pictures

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
 
-
-jQuery.fn.random = function() {
-	var randomIndex = Math.floor(Math.random() * this.length);
-	return jQuery(this[randomIndex]);
-};
-
-
 function loadPics() {
 	var descriptions = "images/profile/descriptions.txt";
 	var container = $('#profile-pic-container');
@@ -15,16 +8,18 @@ function loadPics() {
 
 	jQuery.get(descriptions, function(data, status) {
 		var lines = data.split("\n");
+		var randomIndex = Math.floor(Math.random() * lines.length);
 
 		$.each(lines, function(n, line) {
 			line = line.trim();
-			if (line) {
+			if (randomIndex === n) {
 				var parts = line.split("\t");
 				var path = parts[0];
 				var descr = parts[1];
 				var element = `<img class="${classes}" ${attributes} src="images/profile/${path}" data-content="${descr}">`
-				container.append(element);}
-			});
+				container.append(element);
+			}
+		});
 	});
 };
 
