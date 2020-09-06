fe1f563a365ffb57d782133bd1cc78c1329094ee
Savvas Savvides <savvas.savvides@fortanix.com>
2020-09-06 16:38:59 -0700

    script async

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
-function loadPics() {
+async function loadPics() {
 	const descriptions = "images/profile/descriptions.txt";
 	const container = $('#profile-pic-container');
 	const classes = "profile-pic img-thumbnail";
 	const attributes = 'data-container="body" data-toggle="popover" data-trigger="hover" data-placement="bottom"';
 
-	jQuery.ajax({url: descriptions, async: false, success: function (data) {
-        const lines = data.split("\n");
-
-		// choose a non-empty random line
-		let line = "";
+	// choose a non-empty random line
+	let line = "";
+	await jQuery.get(descriptions, function(data, status) {
+		const lines = data.split("\n");
 		while(!line) {
 			let randomIndex = Math.floor(Math.random() * lines.length);
 			line = lines[randomIndex].trim();
 		}
+	});
 
-		const [path, descr] = line.split("\t");
-		const element = `<img class="${classes}" ${attributes} src="images/profile/${path}" data-content="${descr}">`
-		container.append(element);
-        },
-    });
-
+	const [path, descr] = line.split("\t");
+	const element = `<img class="${classes}" ${attributes} src="images/profile/${path}" data-content="${descr}">`
+	container.append(element);
 };
 
 $(document).ready(function() {
-	loadPics();
+	await loadPics();
 	$('[data-toggle="popover"]').popover();
 });
