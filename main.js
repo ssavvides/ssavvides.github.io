993fa1cba990cbed15e7ccc3cf50ee8d28057aaf
Savvas Savvides <savvas.savvides@fortanix.com>
2020-09-06 15:51:54 -0700

    script debug

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ function loadPics() {
 	// choose a non-empty random line
 	let line = "";
 	const descriptions = "images/profile/descriptions.txt";
+	alert("here");
 	jQuery.get(descriptions, function(data, status) {
+		alert(status);
 		let lines = data.split("\n");
+
 		while(!line) {
 			let randomIndex = Math.floor(Math.random() * lines.length);
 			line = lines[randomIndex].trim();
 		}
-
+		alert(line);
 	});
 
+	alert(line);
 	const container = $('#profile-pic-container');
 	const classes = "profile-pic img-thumbnail";
 	const attributes = 'data-container="body" data-toggle="popover" data-trigger="hover" data-placement="bottom"';
