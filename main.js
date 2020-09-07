7ee04a7737257f29618744ae8e3e2d619914fb5d
Savvas Savvides <savvas.savvides@fortanix.com>
2020-09-06 17:02:16 -0700

    script async

--- main.js
+++ main.js
@@ -4,24 +4,19 @@ function loadPics() {
 	const classes = "profile-pic img-thumbnail";
 	const attributes = 'data-container="body" data-toggle="popover" data-trigger="hover" data-placement="bottom"';
 
-	// choose a non-empty random line
-	let line = "";
-	const promise = jQuery.get(descriptions, function(data, status) {
+	return jQuery.get(descriptions, function(data, status) {
 		const lines = data.split("\n");
+		let line = "";
 		while(!line) {
 			let randomIndex = Math.floor(Math.random() * lines.length);
 			line = lines[randomIndex].trim();
 		}
-	});
-
-	promise.done(function() {
 		const [path, descr] = line.split("\t");
 		const element = `<img class="${classes}" ${attributes} src="images/profile/${path}" data-content="${descr}">`
 		container.append(element);
-	}
+	});
 };
 
 $(document).ready(function() {
-	loadPics();
-	$('[data-toggle="popover"]').popover();
+	loadPics().then($('[data-toggle="popover"]').popover());
 });
